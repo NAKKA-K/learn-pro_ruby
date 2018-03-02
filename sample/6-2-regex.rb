d9f7e2aaea6ce8eedf531621b60c55c29dc7a573
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-03-02 10:36:03 +0000

    Add regex program

---
+++ sample/6-2-regex.rb
@@ -0,0 +1,18 @@
+text = <<TEXT
+I love Python.
+Ruby is a great language.
+Java and JavaScript are different.
+TEXT
+
+text.scan(/[A-Z][A-Za-z]+/) #=> ["Python", "Ruby", "Java", "JavaScript"]
+
+# ある形式の文字列を変換する
+text = <<TEXT
+郵便番号は1234567です
+住所は6770056で 東京都1234です
+TEXT
+p test.gsub(/(\d{3})(\d{4})/, '\1-\2')
+=begin
+郵便番号は123-4567です
+住所は677-0056で 東京都1234です
+=end
