5c2f101520ed02e4409a0778c5e3cbb66188d61c
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-03-10 22:51:43 +0900

    Add

---
+++ sample/7-6-class_extend.rb
@@ -0,0 +1,25 @@
+=begin
+標準ライブラリの継承関係
+BasicObject --- Object --- String
+                        |- Array
+                        |- Hash
+                        |- Numeric --- Integer
+                                    |- Float
+                                    |- Rationnal
+                                    |- Complex
+=end
+
+class User
+end
+
+user = User.new
+user.to_s
+user.nil? #=> false
+
+User.superclass #=> Object
+
+user.methods.sort #=> [:!, :!=, ...]
+
+user.instance_of?(User) #=> true
+user.is_a?(User) #=> true
+user.is_a?(Object) #=> true
